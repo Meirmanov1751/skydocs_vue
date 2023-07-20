a0b0f1ebe746b364856f4b89bfaf0072256a1573
Meirmanov1751 <kassimovd00@mail.com>
2023-07-21 02:37:17 +0600

    skydocs in vue first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="heigth: 100%">
+    <my-header :isMenuHide="isMenuHide" :isProfileHide="isProfileHide" @update-parent-profile="updateParentValueProfile"
+               @update-parent="updateParentValue"></my-header>
+    <div style="display: flex">
+      <my-menu :isMenuHide="isMenuHide" :isHome="isHome" :isProfileHide="isProfileHide"
+               @update-parent-profile="updateParentValueProfile" @update-home="updateHomeValue"
+               @update-parent="updateParentValue"></my-menu>
+      <div style="width: 100%; margin-top: 15px">
+        <router-view></router-view>
+        <div>
+          <div style="padding: 0;"
+               :class="[{'activeMenuHide': isMenuHide || isProfileHide}, {'activeNoMenuHide': !isMenuHide || !isProfileHide}, {'displayNone': isHome}]">
+
+            <div id="content" style="margin: 0px 15px;">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/header/header.js';
+import MyMenu from './components/menu/menu.js';
+import MyHome from './components/home/home.js';
+import Breadcrumbs from './components/loader/loader.js';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    'my-header': MyHeader,
+    'my-home': MyHome,
+    'my-menu': MyMenu,
+    'breadcrumbs': Breadcrumbs,
+  },
+  data() {
+    return {
+      isHome: window.location.hash == "#/home1",
+      isMenuHide: false,
+      isProfileHide: true,
+    };
+  },
+  methods: {
+    updateParentValue(newValue) {
+      console.log(this.isMenuHide)
+      this.isMenuHide = newValue;
+    },
+    updateParentValueProfile(newValue) {
+      console.log(this.isMenuHide)
+      this.isProfileHide = newValue;
+    }, updateHomeValue(newValue) {
+      this.isHome = newValue;
+    },
+  },
 }
 </script>
 
